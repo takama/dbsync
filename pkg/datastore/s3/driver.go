7d946c3cdae03006345b72c8cf499e317fac81e7
Igor Dolzhikov <bluesriverz@gmail.com>
2017-07-26 01:55:11 +0700

    remove unsupported s3 functionality

--- pkg/datastore/s3/driver.go
+++ pkg/datastore/s3/driver.go
@@ -10,7 +10,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	s3svc "github.com/aws/aws-sdk-go/service/s3"
 	"github.com/takama/dbsync/pkg/datastore/binding"
-	"github.com/takama/dbsync/pkg/datastore/mapping"
 )
 
 // S3 driver
@@ -18,18 +17,9 @@ type S3 struct {
 	ctx context.Context
 	*s3svc.Bucket
 	*s3svc.S3
-	json        bool
-	compression bool
-	extension   string
-	bucket      string
-	id          string
-	topics      []string
-	exclude     mapping.Fields
-	spec        mapping.Fields
-	path        mapping.Fields
-	name        mapping.Fields
-	header      mapping.Fields
-	columns     mapping.Fields
+
+	extension string
+	bucket    string
 }
 
 // ErrUnsupported declares error for unsupported methods
@@ -37,24 +27,12 @@ var ErrUnsupported = errors.New("Unsupported method for Amazone S3")
 
 // New creates S3 driver
 func New(
-	accountRegion, accountID, accountKey, accountToken, bucket, id string,
-	json, compression bool, topics []string, extension string,
-	exclude, spec, path, name, header, columns mapping.Fields,
+	accountRegion, accountID, accountKey, accountToken, extension, bucket string,
 ) (db *S3, err error) {
 	db = &S3{
-		ctx:         context.Background(),
-		json:        json,
-		compression: compression,
-		extension:   extension,
-		bucket:      bucket,
-		id:          id,
-		topics:      topics,
-		exclude:     exclude,
-		spec:        spec,
-		path:        path,
-		name:        name,
-		header:      header,
-		columns:     columns,
+		ctx:       context.Background(),
+		extension: extension,
+		bucket:    bucket,
 	}
 	s, err := session.NewSession(&aws.Config{
 		Region:      aws.String(accountRegion),
@@ -70,40 +48,8 @@ func New(
 	return
 }
 
-// LastID implements interface for getting last ID in datastore bucket
-func (db *S3) LastID(bucket string) (id uint64, err error) {
-	bkt, err := db.getBucket(bucket)
-	if err != nil {
-		return
-	}
-	id, err = db.getOrCreateLastID(bkt)
-	if err != nil {
-		return
-	}
-	return id, nil
-}
-
-// Cursor sets pointer
-func (db *S3) Cursor(
-	bucket string, renderMap *mapping.RenderMap, columns []string, values []interface{},
-) (last uint64, err error) {
-
-	return
-}
-
-// AddFromSQL implements interface for inserting data from SQL into bucket
-func (db *S3) AddFromSQL(bucket string, columns []string, values []interface{}) (last uint64, err error) {
-	return 0, ErrUnsupported
-}
-
 // Close flushes data and closes files
 func (db *S3) Close() (err error) {
-	return
-}
-
-// GetFiles should collect
-func (db *S3) GetFiles(path string, fileCount int) (collection map[string]binding.Stream, err error) {
-	err = ErrUnsupported
 	return
 }
 
@@ -126,20 +72,6 @@ func (db *S3) PutFile(path string, stream binding.Stream) error {
 	return nil
 }
 
-// Remove method removes file by path
-func (db *S3) Remove(path string) error {
-	return ErrUnsupported
-}
-
-func (db *S3) getOrCreateLastID(bucket string) (id uint64, err error) {
-	_, err = db.getBucket(bucket)
-	if err != nil {
-		return
-	}
-
-	return
-}
-
 func (db *S3) checkBucket(bucket string) (err error) {
 	result, err := db.S3.ListBuckets(nil)
 	if err != nil {
@@ -160,14 +92,4 @@ func (db *S3) checkBucket(bucket string) (err error) {
 	return db.S3.WaitUntilBucketExists(&s3svc.HeadBucketInput{
 		Bucket: aws.String(bucket),
 	})
-}
-
-func (db *S3) getBucket(name string) (bucket string, err error) {
-	if db.bucket != "" {
-		return db.bucket, nil
-	}
-	r := strings.NewReplacer("-", "", "_", "", " ", "")
-	bucket = r.Replace(name)
-	err = db.checkBucket(bucket)
-	return
 }
